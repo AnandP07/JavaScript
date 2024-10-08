e40298a2135022aef17c9140b225019db01641c5
AnandP07 <115422861+AnandP07@users.noreply.github.com>
2024-10-08 12:39:28 +0000

    variables

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,11 @@
+const accountId = 15544
+let accountEmail = "abc@gmail.com"
+var accountPassword = "1234"
+accountCity = "Jaipur"
+
+
+accountEmail = "sdf@gmail.com"
+accountPassword = "qwe234"
+accountCity = "Noida"
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
