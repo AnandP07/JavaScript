d947c9c3507130471a7018b893ce0e2559875c45
AnandP07 <115422861+AnandP07@users.noreply.github.com>
2024-10-08 12:58:11 +0000

    datatype

--- 01_basics/01_variable.js
+++ 01_basics/01_variable.js
@@ -3,7 +3,10 @@ let accountEmail = "abc@gmail.com"
 var accountPassword = "1234"
 accountCity = "Jaipur"
 
-
+/*
+prefer not to use var 
+because of issue in block scope and functional scope
+*/
 accountEmail = "sdf@gmail.com"
 accountPassword = "qwe234"
 accountCity = "Noida"
